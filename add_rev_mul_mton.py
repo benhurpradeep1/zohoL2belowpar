11f97d7202c5a4953b93f2d77fba215019fdbaff
benhurpradeep1 <55149179+benhurpradeep1@users.noreply.github.com>
2022-01-13 15:20:40 +0530

    Create add_rev_mul_mton.py

---
+++ add_rev_mul_mton.py
@@ -0,0 +1,28 @@
+'''Find all the numbers from M to N that satisfy the following conditions.
+      i. Take a number Z and add all its digits to get a number X.
+      ii. Now reverse the number X to get Y.
+      iii. if we multiply X and Y you should get back Z.'''
+      
+
+
+
+def func(x):
+    sum=0
+    rev=0
+    n=x
+    while (n!=0):
+        rem=n%10
+        sum=sum+rem
+        n = n//10
+    n=sum
+    while (n!=0):
+        rem=n%10
+        rev=(rev*10)+rem
+        n =n // 10
+        
+    return sum*rev==x
+m=int(input())
+n=int(input())
+for i in range (m,n+1):
+    if (func(i)):
+        print(i)
